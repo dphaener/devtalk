96162a223298f0fa74bc7f83ac6727435058e1a8
Darin Haener <dphaener@gmail.com>
2013-11-11 23:25:03 -0800

    All required elements DONE\!

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -55,40 +55,25 @@ angular.module('devtalkApp')
       
       // Save the new message
 
-      $scope.saveMessage = function(newMessage, conversation) {
+      $scope.saveMessage = function(newMessage, conversation, index, isNewMessage) {
         $scope.uLoading = true;
         $http({
-            method: 'POST',
-            url: 'http://geekwise-angularjs.herokuapp.com/darin/projects/' + $scope.currentProject._id + '/conversations/' + conversation._id + '/messages',
-            data : {
-              message: newMessage,
-              user: $scope.currentUser._id
-            }
-            }).success(function (data, status, headers, config) {
-              $scope.uLoading = false;
-              $scope.pLoading = true;
-              $scope.newMessage = '';
-              $scope.isNewMessage = false;
-              $http({
-                method: 'GET',
-                url: 'http://geekwise-angularjs.herokuapp.com/darin/projects'
-                }).success(function (data, status, headers, config) {
-                  $scope.pLoading = false;
-                  $scope.projects = data;
-                  $scope.isEmpty = false;
-                  var i;
-                  for (i = 0; i < $scope.projects.length; i++) {
-                    if ($scope.currentProject._id === $scope.projects[i]._id) {
-                      $scope.currentProject = $scope.projects[i];
-                      $scope.projectSelected = true;
-                    };
-                  };
-                }).error(function (data, status, headers, config) {
-                  
-                });
-            }).error(function (data, status, headers, config) {
+          method: 'POST',
+          url: 'http://geekwise-angularjs.herokuapp.com/darin/projects/' + $scope.currentProject._id + '/conversations/' + conversation._id + '/messages',
+          data : {
+            message: newMessage,
+            user: $scope.currentUser._id
+          }
+        })
+        .success(function (data, status, headers, config) {
+          $scope.uLoading = false;
+          $scope.newMessage = '';
+          $scope.isNewMessage = false;
+          $scope.currentProject.conversations[index].messages.push(data)
+        })
+        .error(function (data, status, headers, config) {
 
-            });
+        });
       };
 
       // Save the new conversation
@@ -96,36 +81,21 @@ angular.module('devtalkApp')
       $scope.saveConversation = function (conversationSubject) {
         $scope.uLoading = true;
         $http({
-            method: 'POST',
-            url: 'http://geekwise-angularjs.herokuapp.com/darin/projects/'+$scope.currentProject._id+'/conversations',
-            data: {
-              subject: conversationSubject
-            }
-            }).success(function (data, status, headers, config) {
-              $scope.newConversation = false;
-              $scope.conversationSubject = '';
-              $scope.pLoading = true;
-              $scope.uLoading = false;
-              $http({
-                method: 'GET',
-                url: 'http://geekwise-angularjs.herokuapp.com/darin/projects'
-              }).success(function (data, status, headers, config) {
-                $scope.pLoading = false;
-                $scope.projects = data;
-                $scope.isEmpty = false;
-                var i;
-                for (i = 0; i < $scope.projects.length; i++) {
-                  if ($scope.currentProject._id === $scope.projects[i]._id) {
-                    $scope.currentProject = $scope.projects[i];
-                    $scope.projectSelected = true;
-                  };
-                };
-              }).error(function (data, status, headers, config) {
-                
-              });
-            }).error(function (data, status, headers, config) {
+          method: 'POST',
+          url: 'http://geekwise-angularjs.herokuapp.com/darin/projects/'+$scope.currentProject._id+'/conversations',
+          data: {
+            subject: conversationSubject
+          }
+        })
+        .success(function (data, status, headers, config) {
+          $scope.newConversation = false;
+          $scope.conversationSubject = '';
+          $scope.uLoading = false;    
+          $scope.currentProject.conversations.push(data);          
+        })
+        .error(function (data, status, headers, config) {
 
-            });
+        });
       };
 
       $scope.projectEdit = function (project) {
@@ -136,7 +106,7 @@ angular.module('devtalkApp')
       $scope.projectNew = function (project) {
         $scope.isEdit = false;
         $scope.open(project);
-      }
+      };
 
       // New Project Dialog
 
@@ -171,8 +141,8 @@ angular.module('devtalkApp')
 		    });
 
 		    modalInstance.result.then(function (project) {
-          console.log(project.description + project.status);
 		    	if (project.isEdit === true) {
+            $scope.pLoading = true;
             $http({
             method: 'PUT',
             url: 'http://geekwise-angularjs.herokuapp.com/darin/projects/'+project._id,
@@ -180,50 +150,43 @@ angular.module('devtalkApp')
               description: project.description,
               status: project.status
             }
-            }).success(function (data, status, headers, config) {
-              $scope.pLoading = true;
-              $http({
-                method: 'GET',
-                url: 'http://geekwise-angularjs.herokuapp.com/darin/projects'
-                }).success(function (data, status, headers, config) {
-                  $scope.projects = data;
-                  $scope.isEmpty = false;
-                  $scope.pLoading = false;
-                  $scope.currentProject = project;
-                  $scope.projectSelected = true;
-                }).error(function (data, status, headers, config) {
-                  
-                });
-            }).error(function (data, status, headers, config) {
+          })
+          .success(function (data, status, headers, config) {
+            $scope.isEmpty = false;
+            $scope.pLoading = false;
+            $scope.currentProject = data[0];
+            $scope.projectSelected = true;
+          })
+          .error(function (data, status, headers, config) {
 
-            });
+          });
           } else {
-            delete project.isEdit;  
+            $scope.pLoading = true;
+            var i;
+            var projectTeam = [];
+
+            for (i = 0; i < project.team.length; i++) {
+              projectTeam.push(project.team[i]._id);
+            };
+
             $http({
-            method: 'POST',
-            url: 'http://geekwise-angularjs.herokuapp.com/darin/projects',
-            data : {
-              title: project.title,
-              description: project.description,
-              status: project.status,
-              dueDate: project.dueDate,
-              team: project.team
-            }
-            }).success(function (data, status, headers, config) {
-              $scope.pLoading = true;
-              $http({
-                method: 'GET',
-                url: 'http://geekwise-angularjs.herokuapp.com/darin/projects'
-                }).success(function (data, status, headers, config) {
-                  $scope.projects = data;
-                  $scope.isEmpty = false;
-                  $scope.pLoading = false;
-                  $scope.currentProject = project;
-                  $scope.projectSelected = true;
-                }).error(function (data, status, headers, config) {
-                  
-                });
-            }).error(function (data, status, headers, config) {
+              method: 'POST',
+              url: 'http://geekwise-angularjs.herokuapp.com/darin/projects',
+              data: {
+                title: project.title,
+                description: project.description,
+                status: project.status,
+                dueDate: project.dueDate,
+                team: projectTeam
+              }
+            })
+            .success(function (data, status, headers, config) {
+              $scope.pLoading = false;
+              $scope.isEmpty = false;
+              $scope.currentProject = data[0];
+              $scope.projectSelected = true;
+            })
+            .error(function (data, status, headers, config) {
 
             });
           };
@@ -242,6 +205,44 @@ angular.module('devtalkApp')
             });
         });
 		  };
+
+      // The edit conversation modal
+
+      $scope.editConvOpen = function (conversation) {
+        var modalInstance = $modal.open({
+          templateUrl: 'views/templates/newConversationModal.html',
+          controller: ConvModalInstanceCtrl,
+          backdrop: 'static',
+          windowClass: 'new-project',
+          resolve: {
+            subject: function () {
+              return conversation.subject;
+            }
+          }
+        });
+
+        modalInstance.result.then(
+        function (newSubject) {
+          $scope.uLoading = true;
+          $http({
+            method: 'PUT',
+            url: 'http://geekwise-angularjs.herokuapp.com/darin/conversations/'+conversation._id,
+            data: {
+              subject: newSubject
+            }
+          })
+          .success(function (data, status, headers, config) {
+            conversation.subject = newSubject;
+            $scope.isEmpty = false;
+            $scope.uLoading = false;
+            $scope.projectSelected = true;
+          })
+          .error(function (data, status, headers, config) {
+
+          });
+        });
+        conversation.open = true;
+      };
   })
   .filter('fromNow', function() {
     return function(dateString) {
@@ -256,7 +257,7 @@ var ModalInstanceCtrl = function ($scope, $modalInstance, users, project, isEdit
   $scope.isEdit = isEdit;
   $scope.project = project;
   $scope.users = users;
-  $scope.items = ['New', 'In Progess', 'Completed'];
+  $scope.items = ['Not Started', 'On Track', 'Delayed', 'Completed'];
   $scope.project.status = $scope.items[0];
   $scope.selectedUser = $scope.users[0];
   
@@ -298,4 +299,19 @@ var ModalInstanceCtrl = function ($scope, $modalInstance, users, project, isEdit
       $scope.userAbsent = true;
     };
   }
+};
+
+var ConvModalInstanceCtrl = function ($scope, $modalInstance, subject) {
+
+  $scope.newSubject = subject;
+
+  $scope.ok = function (newSubject) {
+    $modalInstance.close(newSubject);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+
+  
 };
